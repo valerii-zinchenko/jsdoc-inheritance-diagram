933d04cda00726d43462a799c2ea47bbbfd15584
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2018-02-09 00:30:52 +0200

    chore: update setup for an example

---
+++ example/example2.js
@@ -0,0 +1,66 @@
+/**
+ * ChildL11
+ * @class
+ * @augments Root
+ */
+function ChildL11() {}
+/**
+ * Overriden metod
+ *
+ * @param {String} str - String
+ */
+ChildL11.prototype.method = function(){};
+/**
+ * Method 2
+ */
+ChildL11.prototype.method2 = function(){};
+
+/**
+ * ChildL12
+ * @class
+ * @augments Root
+ */
+function ChildL12() {}
+
+/**
+ * ChildL21
+ * @class
+ * @augments ChildL11
+ * @mixes Mixin1
+ * @mixes Mixin2
+ * @mixes ExtMixin
+ */
+function ChildL21(){}
+/**
+ * Method 3
+ */
+ChildL21.prototype.method3 = function(){};
+/**
+ * Property 3
+ *
+ * @type {String}
+ */
+ChildL21.prototype.prop3 = 'str';
+
+/**
+ * Mixin class
+ * @class
+ */
+function Mixin1(){}
+/**
+ * mixin method 1
+ */
+Mixin1.prototype.mixin1method = function(){};
+/**
+ * property of mixin 1
+ */
+Mixin1.prototype.mixin1prop = 'str';
+/**
+ * Mixin2
+ * @class
+ */
+function Mixin2(){}
+/**
+ * mixin method 2
+ */
+Mixin2.prototype.mixin2method = function(){};
